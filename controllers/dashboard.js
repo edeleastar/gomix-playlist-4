279024d4e28a614203e343d7b72e5f55f7ca009e
Eamonn de Leastar <edeleastar@wit.ie>
2017-01-03 10:45:13 +0000

    moved the playlist to playlist-store module

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,24 +1,7 @@
 'use strict';
 
 const logger = require('../utils/logger');
-
-const sonatas = {
-  title: 'Beethoven Sonatas',
-  songs: [
-    {
-      title: 'Piano Sonata No. 3',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 7',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 10',
-      artist: 'Beethoven',
-    },
-  ],
-};
+const sonatas = require('../models/playlist-store.js');
 
 const dashboard = {
   index(request, response) {
