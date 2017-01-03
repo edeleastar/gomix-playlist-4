af8092e3ebef340af2b8569090be5b342bb827d5
Eamonn de Leastar <edeleastar@wit.ie>
2017-01-03 14:57:31 +0000

    rename playlist to playlistcollection

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const logger = require('../utils/logger');
-const playlists = require('../models/playlist-store.js');
+const playlistCollection = require('../models/playlist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
       title: 'Playlist Dashboard',
-      playlists: playlists,
+      playlists: playlistCollection,
     };
-    logger.info('about to render', playlists);
+    logger.info('about to render', playlistCollection);
     response.render('dashboard', viewData);
   },
 };
