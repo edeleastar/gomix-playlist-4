bb9cd13d96b48868b153016d245539449c45540c
Eamonn de Leastar <edeleastar@wit.ie>
2017-01-03 11:17:42 +0000

    refactor to display multiple playlists

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const logger = require('../utils/logger');
-const sonatas = require('../models/playlist-store.js');
+const playlists = require('../models/playlist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
       title: 'Playlist Dashboard',
-      playlist: sonatas,
+      playlists: playlists,
     };
-    logger.info('about to render', sonatas);
+    logger.info('about to render', playlists);
     response.render('dashboard', viewData);
   },
 };
