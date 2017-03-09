b126ca8b18420df608a839f0d6b88e91976c3f84
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-09 19:12:35 +0000

    routes to match accounts links

--- routes.js
+++ routes.js
@@ -7,8 +7,14 @@ const start = require('./controllers/start');
 const dashboard = require('./controllers/dashboard.js');
 const playlist = require('./controllers/playlist.js');
 const about = require('./controllers/about.js');
+const accounts = require ('./controllers/accounts.js');
 
-router.get('/', start.index);
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
 
 router.get('/dashboard', dashboard.index);
 router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
